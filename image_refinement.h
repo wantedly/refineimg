39162525a2bed7b60162d2b3acb3b8e884177ca1
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2016-11-09 23:51:03 +0900

    Modify again

--- image_refinement.h
+++ image_refinement.h
@@ -1,6 +1,8 @@
 #ifndef YASHMA_IMAGE_REFINEMENT_H_
 #define YASHMA_IMAGE_REFINEMENT_H_
 
+#include <vector>
+
 std::vector<unsigned char> refine(const std::vector<unsigned char>& img);
 
 #endif // YASHMA_IMAGE_REFINEMENT_H_
