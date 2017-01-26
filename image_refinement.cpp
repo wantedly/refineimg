402c059ace964932f483e6b8b4926700de3a478f
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2017-01-26 19:27:02 +0900

    Solve yellowed card problem

--- image_refinement.cpp
+++ image_refinement.cpp
@@ -29,7 +29,6 @@ cv::Mat refine(const cv::Mat& src, cv::Mat* hist_image) {
   for (int c = 0; c < 3; c++) {
     cv::calcHist(&bgr_planes[c], 1, 0, cv::Mat(), hist[c], 1, &histSize, &histRange, uniform, accumulate);
     int cnt = 0;
-    bool first = true;
     for (int i = histSize - 1; i >= 0; i--) {
       int v = hist[c].at<float>(i);
       if (cnt < high_bar && cnt+v >= high_bar) high01[c] = i;
@@ -38,21 +37,41 @@ cv::Mat refine(const cv::Mat& src, cv::Mat* hist_image) {
     }
   }
 
-  int low = min(min(128, low01[0]), min(low01[1], low01[2]));
-  int high = max(max(128, high01[0]), max(high01[1], high01[2]));
-  vector<unsigned char> lut(256);
+  int lowest = min(low01[0], min(low01[1], low01[2]));
+  int highest = max(high01[0], max(high01[1], high01[2]));
+  int low_max = lowest * 1.25;
+  int high_min = highest * 0.8;
+  int lowv[3], highv[3];
+
+  for (int c = 0; c < 3; c++) {
+    lowv[c] = min(low01[c], min(127, low_max));
+    highv[c] = max(high01[c], max(128, high_min));
+  }
+
+  cv::Mat lut(256, 1, CV_8UC3);
   int l = -4, h = 5;
   double ll = 1.0 / (1 + pow(2, -l));
   double hh = 1.0 / (1 + pow(2, -h));
 
   for (int i = 0; i < 256; i++) {
-    double x = 1.0 * (i - low) / (high - low) * (h - l) + l;
-    double y = (1.0 / (1.0 + pow(2.0, -x)) - ll) / (hh - ll);
-    // cout << low << " " << high << " " << i << " " << x << " " << y << endl;
-    lut[i] = max(0.0, min(255.0, 255.0 * y));
+    unsigned char *row = lut.ptr<unsigned char>(i);
+    for (int c = 0; c < 3; c++) {
+      double x = 1.0 * (i - lowv[c]) / (highv[c] - lowv[c]) * (h - l) + l;
+      double y = (1.0 / (1.0 + pow(2.0, -x)) - ll) / (hh - ll);
+      // cout << lowv[c] << " " << highv[c] << " " << i << " " << x << " " << y << endl;
+      row[c] = max(0.0, min(255.0, 255.0 * y));
+    }
   }
   cv::Mat dst;
   cv::LUT(src, lut, dst);
+
+  cv::Mat hsv;
+  cv::cvtColor(dst, hsv, CV_BGR2HSV);
+  vector<cv::Mat> panes;
+  split(hsv, panes);  // split to H, S, V
+  panes[1] *= (255.0 - (min(127, lowest) + 255 - max(128, highest)) * 0.2) / 255.0;
+  merge(panes, hsv);
+  cvtColor(hsv, dst, CV_HSV2BGR);
 
   if (hist_image != NULL) {
     // Draw the histograms for B, G and R
@@ -78,7 +97,11 @@ cv::Mat refine(const cv::Mat& src, cv::Mat* hist_image) {
       cv::circle(*hist_image, cv::Point(bin_w*high01[c], hist_h*0.3), 10, colors[c], 2);
     }
     for (int i = 1; i < histSize; i++) {
-      cv::line(*hist_image, cv::Point(bin_w*(i-1), hist_h * (255 - lut[i-1]) / 255.0), cv::Point(bin_w*(i), hist_h * (255 - lut[i]) / 255.0), cv::Scalar(0, 255, 255), 1);
+      const unsigned char *prev = lut.ptr<unsigned char>(i-1);
+      const unsigned char *cur = lut.ptr<unsigned char>(i);
+      for (int c = 0; c < 3; c++) {
+        cv::line(*hist_image, cv::Point(bin_w*(i-1), hist_h * (255 - prev[c]) / 255.0), cv::Point(bin_w*(i), hist_h * (255 - cur[c]) / 255.0), cv::Scalar(0, 255, 255), 1);
+      }
     }
   }
   return dst;
